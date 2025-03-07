0a08f16e5d6271f821204329ff77d9894be4cd18
gaaelgarciia <gael.garciaa@udc.es>
2025-03-07 19:21:00 +0100

    README para aclarar como ejecutarlo y alguna prueba mas en el ejecutable.sh

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 Repositorio dedicado a las practicas de la asignatura de Representacion do Coñecemento e Razoamento Automático de la Facultad de Informática de la Coruña.
 
+Para ejecutar la prueba de todos los codigos proporcionados ejecuta lo siguiente en terminal 
+
+```bash
+chmod +x ejecutable.sh
+./ejecutable.sh
+```
+
+Esto encodeara, decadeara y hara un diff para comprobar que todas las soluciones son correctas
--- ejecutable.sh
+++ ejecutable.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+echo "Enconding files..."
 
+python3 encode.py examples/examples/dom00.txt dom00.lp
+python3 encode.py examples/examples/dom01.txt dom01.lp
+python3 encode.py examples/examples/dom02.txt dom02.lp
+python3 encode.py examples/examples/dom03.txt dom03.lp
+python3 encode.py examples/examples/dom04.txt dom04.lp
+python3 encode.py examples/examples/dom05.txt dom05.lp
+python3 encode.py examples/examples/dom06.txt dom06.lp
+python3 encode.py examples/examples/dom07.txt dom07.lp
+python3 encode.py examples/examples/dom08.txt dom08.lp
+python3 encode.py examples/examples/dom09.txt dom09.lp
+python3 encode.py examples/examples/dom10.txt dom10.lp
+python3 encode.py examples/examples/dom11.txt dom11.lp
+
+echo "Decoding files..."
+
+python3 decode.py yinyangKB.lp dom00.lp sol00.txt
 python3 decode.py yinyangKB.lp dom01.lp sol01.txt
 python3 decode.py yinyangKB.lp dom02.lp sol02.txt
 python3 decode.py yinyangKB.lp dom03.lp sol03.txt
